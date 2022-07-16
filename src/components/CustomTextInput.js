bc2ae494051eef7dab3f90e0ed7056acf5629cb1
Muneeb <muneebrxxx@gmail.com>
2022-07-17 04:05:51 +0500

    Password show icon and secure text added

--- src/components/CustomTextInput.js
+++ src/components/CustomTextInput.js
@@ -1,5 +1,12 @@
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import {
   black,
   grey,
@@ -11,7 +18,17 @@ import validateText from "../utils/Validation";
 const CustomTextInput = (props) => {
   const [text, setText] = useState("");
   const [error, setError] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isSecureEntry, setIsSecureEntry] = useState(false);
   const [errorText, setErrorText] = useState("*This field is required");
+
+  useEffect(() => {
+    if (props.type.toLowerCase() === "password") {
+      console.log("GREAT");
+      setIsPassword(true);
+      setIsSecureEntry(true);
+    }
+  }, []);
 
   const onBlur = () => {
     if (props.required) {
@@ -29,14 +46,36 @@ const CustomTextInput = (props) => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={[styles.input, error && styles.inputError]}
-        value={text}
-        placeholderTextColor="#afafaf"
-        placeholder={props.type}
-        onBlur={onBlur}
-        onChange={onChange}
-      />
+      <View style={styles.eyeContainer}>
+        <TextInput
+          style={[
+            styles.input,
+            error && styles.inputError,
+            !isPassword && { marginRight: marginHorizontal },
+          ]}
+          value={text}
+          placeholderTextColor="#afafaf"
+          placeholder={props.type}
+          onBlur={onBlur}
+          onChange={onChange}
+          secureTextEntry={isSecureEntry}
+        />
+        {isPassword && (
+          <View style={[styles.eye, error && styles.inputError]}>
+            <TouchableOpacity
+              onPress={() => {
+                setIsSecureEntry(!isSecureEntry);
+              }}
+            >
+              <FontAwesome5
+                name={isSecureEntry ? "eye" : "eye-slash"}
+                size={20}
+                color="gray"
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
       {error && <Text style={styles.error}>{errorText}</Text>}
     </View>
   );
@@ -44,12 +83,16 @@ const CustomTextInput = (props) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     alignSelf: "stretch",
   },
   input: {
+    flex: 1,
     color: black,
     marginTop: marginVertical,
-    marginHorizontal: marginHorizontal,
+    marginLeft: marginHorizontal,
     paddingBottom: 5,
     fontSize: 16,
     borderBottomWidth: 0.5,
@@ -59,9 +102,22 @@ const styles = StyleSheet.create({
     borderBottomColor: "red",
   },
   error: {
+    alignSelf: "flex-start",
     color: "red",
     marginVertical: 5,
     marginHorizontal: marginHorizontal,
+  },
+  eye: {
+    paddingRight: 5,
+    paddingVertical: 6,
+    marginRight: marginHorizontal,
+    alignSelf: "flex-end",
+    borderBottomWidth: 0.5,
+    borderBottomColor: grey,
+  },
+  eyeContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
   },
 });
 
