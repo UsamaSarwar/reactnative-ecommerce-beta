9525106484696f1dc605dfb1c6aae5e6bb8fb97d
Muneeb <muneebrxxx@gmail.com>
2022-07-19 11:20:41 +0500

    signup, renaming user schema, added new modules

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -1,4 +1,30 @@
-export const signUp = (req, res) => {};
+import user from "../model/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const signUp = async (req, res) => {
+  try {
+    let hash = await bcrypt.hash(req.body.password, 10);
+    if (req.body.email && req.body.username) {
+      await user.create({
+        email: req.body.email,
+        name: req.body.name,
+        password: hash,
+        phoneNo: req.body.phone,
+        dob: req.body.dob,
+        address: req.body.address ? req.body.address : "",
+      });
+      res
+        .status(200)
+        .json({ header: { message: "Sign Up successfull" }, body: {} });
+    }
+  } catch (err) {
+    res.status(500).json({
+      header: { message: err.message },
+      body: {},
+    });
+  }
+};
 
 export const signIn = (req, res) => {};
 
--- API/model/user.js
+++ API/model/user.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  email: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  phoneNo: { type: String, required: true },
+  phone: { type: String, required: true },
   dob: { type: Date, required: true },
-  Address: { type: String, required: false },
+  address: { type: String, required: false },
 });
 
-export default userSchema;
+export default mongoose.model("User", userSchema);
