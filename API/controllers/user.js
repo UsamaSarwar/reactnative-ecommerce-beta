46c5d0376268b869f4cee3fa32ea24aa09034765
Muneeb <muneebrxxx@gmail.com>
2022-07-19 15:46:16 +0500

    Sign in and Sign Up API

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -1,22 +1,30 @@
 import user from "../model/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import moment from "moment";
 
 export const signUp = async (req, res) => {
   try {
+    const result = await user.findOne({ email: req.body.email });
+    if (result) {
+      throw new Error("ID already exists");
+    }
     let hash = await bcrypt.hash(req.body.password, 10);
-    if (req.body.email && req.body.username) {
+    if (req.body.email && req.body.name) {
       await user.create({
         email: req.body.email,
         name: req.body.name,
         password: hash,
-        phoneNo: req.body.phone,
-        dob: req.body.dob,
+        phone: req.body.phone,
+        dob: moment(req.body.dob),
         address: req.body.address ? req.body.address : "",
       });
-      res
-        .status(200)
-        .json({ header: { message: "Sign Up successfull" }, body: {} });
+      res.status(200).json({
+        header: { message: "Sign Up successfull" },
+        body: {},
+      });
+    } else {
+      throw new Error("missing email or name values");
     }
   } catch (err) {
     res.status(500).json({
@@ -26,7 +34,48 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const signIn = (req, res) => {};
+export const signIn = async (req, res) => {
+  try {
+    const result = await user.findOne({ email: req.body.email });
+    if (result) {
+      bcrypt.compare(req.body.password, result.password, (err, success) => {
+        if (err) {
+          throw new Error(err);
+        }
+        if (success) {
+          const token = jwt.sign(
+            {
+              id: result._id,
+              username: req.body.username,
+            },
+            process.env.JWT_SECRET
+          );
+          res.status(418).json({
+            header: { message: "success" },
+            body: {
+              token: token,
+            },
+          });
+        } else {
+          res.status(200).json({
+            header: { message: "Invalid credentials" },
+            body: {},
+          });
+        }
+      });
+    } else {
+      res.status(401).json({
+        header: { message: "Invalid credentials" },
+        body: {},
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      header: { message: err.message },
+      body: {},
+    });
+  }
+};
 
 export const updatePassword = (req, res) => {};
 
